e3e7bd2f97229e6b84f95aaebccff38251929817
anshikasaxena50 <anshikasaxena50@gmail.com>
2026-09-10 20:54:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-async function askAI() {
+function formatAnswer(text) {
+    return text
+        .replace(/^### (.*)$/gm, "<h3>$1</h3>")
+        .replace(/^## (.*)$/gm, "<h2>$1</h2>")
+        .replace(/^# (.*)$/gm, "<h1>$1</h1>")
+        .replace(/\*\*\*(.*?)\*\*\*/g, "<strong><em>$1</em></strong>")
+        .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
+        .replace(/\*(.*?)\*/g, "<em>$1</em>")
+        .replace(/^---$/gm, "<hr>")
+        .replace(/^\d+\.\s+(.*)$/gm, "<div class='list-item'>• $1</div>")
+        .replace(/^\*\s+(.*)$/gm, "<div class='list-item'>• $1</div>")
+        .replace(/\n\n/g, "<br><br>")
+        .replace(/\n/g, "<br>");
+}
+async function askAI(answer.innerHTML = formatAnswer(data.answer);) {
     const question = document.getElementById("question").value;
     const answer = document.getElementById("answer");
 
@@ -25,7 +39,7 @@ async function askAI() {
         if (data.error) {
             answer.innerText = "Error: " + data.error;
         } else {
-            answer.innerText = data.answer;
+            answer.innerHTML = formatAnswer(data.answer);
         }
 
     } catch (error) {
